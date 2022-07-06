ab2351523753000184336fe90573e03ef1de15fd
ahahahw <funcrafter1337@gmail.com>
2022-07-06 10:56:34 +0300

    lol

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/test','App\Http\Controllers\TestController');
+
+Route::get('/','App\Http\Controllers\TestController');

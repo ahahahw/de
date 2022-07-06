585ac77554f14a18c9e2b7d28413022441fa7bf6
ahahahw <funcrafter1337@gmail.com>
2022-07-06 10:57:44 +0300

    a

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/','App\Http\Controllers\TestController');
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/test','App\Http\Controllers\TestController');
